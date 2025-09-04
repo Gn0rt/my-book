7def62f5812a84cd047639376567ee9f13535acf
Gn0rt <108060689+Gn0rt@users.noreply.github.com>
2025-09-04 11:47:32 +0700

    check login infomation

--- frontend/src/pages/Auth/Login.vue
+++ frontend/src/pages/Auth/Login.vue
@@ -1,9 +1,15 @@
 <script setup>
 import ImgLogin from "@/assets/images/login.png";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const props = defineProps(['users']);
 const userStorage = props.users;
+
+const checkEmail = ref(false);
+const checkPassword = ref(false);
+
+const errorEmail = ref('');
+const errorPassword = ref('');
 
 const formData = ref({
   emailLogin: '',
@@ -14,14 +20,31 @@ const formData = ref({
 const handleSubmit = () => {
   if(!formData.value.emailLogin.trim()) {
     console.log("Email không được để trống!");
+    checkEmail.value = true;
+    errorEmail.value = "Email không được để trống!";
   }else if(!/^\S+@\S+\.\S+$/.test(formData.value.emailLogin)) {
+    checkEmail.value = true;
+    errorEmail.value = "Email sai định dạng!";
     console.log("Email sai định dạng");
   }
 
   if(!formData.value.passwordLogin.trim()) {
+    checkPassword.value = true;
+    errorPassword.value = "Password không được để trống!";
     console.log("Password không được để trống!");
   }
 }
+
+watch(() => [ formData.value.emailLogin, formData.value.passwordLogin], ([newEmail, newPassword]) => {
+  if(newEmail.trim()) {
+    checkEmail.value = false;
+    errorEmail.value = '';
+  }
+  if(newPassword.trim()) {
+    checkPassword.value = false;
+    errorPassword.value = '';
+  }
+})
 </script>
 
 <template>
@@ -36,23 +59,27 @@ const handleSubmit = () => {
       <h2 class="text-2xl font-bold text-gray-800 mb-6">Đăng nhập</h2>
 
       <!-- Tên đăng nhập -->
-      <div class="flex items-center mb-5">
-        <label class="w-32 text-gray-700">Email: </label>
+      <div class="flex items-center mb-5 flex-wrap relative">
         <input
           v-model="formData.emailLogin"
           type="text"
-          class="ml-3 flex-1 p-2 border border-gray-300 rounded-lg outline-none focus:border-[#FF971D] transition-colors"
+          placeholder=" "
+          class="inp ml-3 flex-1 p-2 border border-gray-300 rounded-lg outline-none focus:border-[#FF971D] transition-colors"
         />
+        <label class="text-label bg-white text-gray-300 absolute top-2 left-8">Email</label>
+        <p v-if="checkEmail" class="ml-4 text-xs text-red-500 w-full">{{ errorEmail }}</p>
       </div>
 
       <!-- Mật khẩu -->
-      <div class="flex items-center mb-6">
-        <label class="w-32 text-gray-700">Mật khẩu:</label>
+      <div class="flex items-center mb-6 mt-5 flex-wrap relative">
         <input
           v-model="formData.passwordLogin"
           type="password"
-          class="ml-3 flex-1 p-2 border border-gray-300 rounded-lg outline-none focus:border-[#FF971D] transition-colors"
+          placeholder=" "
+          class="inp ml-3 flex-1 p-2 border border-gray-300 rounded-lg outline-none focus:border-[#FF971D] transition-colors"
         />
+        <label class="text-label bg-white text-gray-300 absolute top-2 left-8">Mật khẩu</label>
+        <p v-if="checkPassword" class="ml-4 text-xs text-red-500 w-full">{{ errorPassword }}</p>
       </div>
 
       <!-- Nút đăng nhập -->
@@ -65,3 +92,19 @@ const handleSubmit = () => {
     </form>
   </div>
 </template>
+
+<style scoped>
+.text-label {
+  transition: transform 0.2s ease, color 0.2s;
+}
+.inp:focus+.text-label {
+  transform: translateY(-22px);
+  color: #000;
+}
+/* Hiển thị label ở trên nếu input có nội dung (không rỗng) */
+.inp:not(:placeholder-shown) + .text-label {
+  transform: translateY(-22px);
+  color: #000;
+}
+
+</style>
